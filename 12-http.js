f628bccfd1657360d42f4bc94d3a01f78d7b6472
Frank Lim <franklim@Franks-iMac.local>
2023-03-14 16:38:33 +0800

    first commit

---
+++ 12-http.js
@@ -0,0 +1,12 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.end("Welcome to our home page");
+  }
+  if (req.url === "/about") {
+    res.end("This is our short history");
+  } else res.end("fuck");
+});
+
+server.listen(5623);
